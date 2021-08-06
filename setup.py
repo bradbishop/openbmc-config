a539001b75b00f74347fa6f3d6cbfe6cf57a6605
Andrew Jeffery <andrew@aj.id.au>
2021-08-06 09:33:06 +0930

    Introduce setuptools
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name="openbmc-subtree",
+        version="0.1.0",
+        author="Brad Bishop",
+        author_email="bradleyb@fuzziesquirrel.com",
+        description="openbmc-subtree manipulates layer dependencies of OpenBMC",
+        url="https://github.com/bradbishop/openbmc-config",
+        scripts=['openbmc-subtree']
+)
